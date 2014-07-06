4e7116dc1ffe83403bf20641096233e1a771f28b
David Brownman <beamneocube@gmail.com>
2014-07-06 02:39:18 -0700

    inital commit

---
+++ spec/test.rb
@@ -0,0 +1,27 @@
+ENV['RACK_ENV'] = 'test'
+require 'minitest/autorun'
+require 'rack/test'
+
+require_relative '../sinatra-rocket.rb'
+include Rack::Test::Methods
+
+class SinatraRocketTest < MiniTest::Test
+  def app
+    Sinatra::Application
+  end
+
+  def loads
+    get '/'
+    assert last_response.ok?
+  end
+
+  def test_it_says_hello_world
+    get '/'
+    assert last_response.body.include? 'Hello, World!'
+  end
+
+  def test_it_says_hello_to_a_person
+    get '/info'
+    assert last_response.body.include? "Information"
+  end
+end
